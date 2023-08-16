1f1c88ef3735c4c92590ed3e15358f1c60e7909e
INSP1RIT <inspiritstillok@protonmail.com>
2023-08-17 00:49:28 +0300

    v1api

--- app.js
+++ app.js
@@ -4,21 +4,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({message: 'Hello from the server side!', app:'Natours'}
-// )
-// })
-//
-// app.post('/', (req, res) => {
-//     res.end('You can post to this endpoint');
-// })
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
 app.use(express.json());
 
-app.get('/api/v1/tours', (req, res) => {
+function getAllTours(req, res) {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -26,29 +18,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+}
 
-
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-
+function getTour(req, res) {
   const id = +req.params.id;
-  const tour = tours.find(el => el.id === id)
+  const tour = tours.find((el) => el.id === id);
 
   if (id > tours.length) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Wrong id passed'
-    })
+      message: 'Wrong id passed',
+    });
   }
 
   res.status(200).json({
     status: 'success',
-    tour
-  })
-})
+    tour,
+  });
+}
 
-app.post('/api/v1/tours', (req, res) => {
+function createTour(req, res) {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -65,7 +54,55 @@ app.post('/api/v1/tours', (req, res) => {
       });
     },
   );
-});
+}
+
+function updateTour(req, res) {
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Wrong id passed',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<h1>Updated</h1>',
+    },
+  });
+}
+
+function deleteTour(req, res) {
+  if (req.params.id > tours.length) {
+    return res.status(204).json({
+      status: 'fail',
+      message: 'Wrong id passed',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+}
+
+// //app.get('/api/v1/tours', getAllTours);
+//
+// app.get('/api/v1/tours/:id', getAllTour);
+//
+// // app.post('/api/v1/tours', createTour);
+//
+// app.patch('/api/v1/tours/:id', updateTour);
+//
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .g  et(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
